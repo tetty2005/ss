33ae454fca259e6b733bb739219c5e22144fa821
tetty2005 <tetty2005@mail.ru>
2017-02-10 18:12:08 +0200

    Update script.js

--- Budget/js/script.js
+++ Budget/js/script.js
@@ -1,40 +1,58 @@
 var wastes = {};
 
 function myFunction(wasteType) {
-  var waste = document.getElementById('input_' + wasteType).value;
-  wastes[wasteType] = wastes[wasteType] || [];
-  wastes[wasteType].push(waste*1);
-  calculate(wastes, wasteType);
-  console.log(wastes);
-  save(wastes);
+	var waste = document.getElementById('input_' + wasteType).value;
+	wastes[wasteType] = wastes[wasteType] || [];
+	wastes[wasteType].push(waste*1);
+	calculate(wastes, wasteType);
+	console.log(wastes);
+	save(wastes);
 }
 
 function save(wastes) {
-  localStorage.setItem("wastes", JSON.stringify(wastes));
+	localStorage.setItem("wastes", JSON.stringify(wastes));
 }
 
 window.onload = function() {
-  wastes = JSON.parse(localStorage.getItem("wastes")) || {};
-  for (wasteType in wastes) {
-    calculate(wastes, wasteType);
-  }
+	wastes = JSON.parse(localStorage.getItem("wastes")) || {};
+	for (wasteType in wastes) {
+		calculate(wastes, wasteType);
+	}
 }
 
 function calculate(wastes, wasteType){
     var sum = 0;
     for (i = 0; i < wastes[wasteType].length; i++) {
-      sum += wastes[wasteType][i];
+    	sum += wastes[wasteType][i];
     }
     document.getElementById('span_' + wasteType).innerHTML = sum;
     
     var max = document.getElementById('progress_' + wasteType).max;
     var balance = max - sum;
-    if (balance > max) {
-      document.getElementById('balance_' + wasteType).style.color = 'red';
-      document.getElementById('balance_' + wasteType).innerHTML = balance;
+    document.getElementById('balance_' + wasteType).innerHTML = balance;
+    if (balance < 0) {
+		document.getElementById('balance_' + wasteType).style.color = 'red';
+		// document.getElementById('progress_' + wasteType).style.progress::-webkit-progress-value = 'red';
     }
     else {
-      document.getElementById('balance_' + wasteType).innerHTML = balance;
+	
+
     }
     document.getElementById('progress_' + wasteType).value = sum;
-  }
+}
+
+function plus1(wasteType){
+	var waste = 1;
+	wastes[wasteType].push(waste*1);
+	calculate(wastes, wasteType);
+	console.log(wastes);
+	save(wastes);
+}
+
+function plus10(wasteType){
+	var waste = 10;
+	wastes[wasteType].push(waste*1);
+	calculate(wastes, wasteType);
+	console.log(wastes);
+	save(wastes);
+}
