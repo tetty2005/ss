6dffd60a7ecabc6b0ad48f18812692bbabd50d7f
Tatiana <tetty2005@mail.ru>
2017-03-02 11:40:05 +0200

    Update script.js

--- Budget/js/script.js
+++ Budget/js/script.js
@@ -1,23 +1,28 @@
+var wasteTypes = ['daily', 'house', 'social', 'sport', 'style'];
 var wastes = {};
+var maxes = {};
 
 function myFunction(wasteType) {
 	var waste = document.getElementById('input_' + wasteType).value;
 	wastes[wasteType] = wastes[wasteType] || [];
 	wastes[wasteType].push(waste*1);
 	calculate(wastes, wasteType);
-	console.log(wastes);
-	save(wastes);
+	save();
 }
 
-function save(wastes) {
+function save() {
+	localStorage.setItem("maxes", JSON.stringify(maxes));
 	localStorage.setItem("wastes", JSON.stringify(wastes));
 }
 
 window.onload = function() {
+	maxes = JSON.parse(localStorage.getItem("maxes")) || {};
 	wastes = JSON.parse(localStorage.getItem("wastes")) || {};
-	for (wasteType in wastes) {
+
+	wasteTypes.forEach(function (wasteType){
+		setMax(wasteType, maxes[wasteType] || 0);
 		calculate(wastes, wasteType);
-	}
+	});
 }
 
 function calculate(wastes, wasteType){
@@ -25,27 +30,36 @@ function calculate(wastes, wasteType){
     for (i = 0; i < wastes[wasteType].length; i++) {
     	sum += wastes[wasteType][i];
     }
+    document.getElementById('progress_' + wasteType).value = sum;
     document.getElementById('span_' + wasteType).innerHTML = sum;
     
-    var max = document.getElementById('progress_' + wasteType).max;
-    var balance = max - sum;
+    var balance = maxes[wasteType] - sum;
     document.getElementById('balance_' + wasteType).innerHTML = balance;
-    if (balance < 0) {
-		document.getElementById('balance_' + wasteType).style.color = 'red';
-		// document.getElementById('progress_' + wasteType).style.progress::-webkit-progress-value = 'red';
-    }
-    else {
 	
+	document.getElementById('balance_' + wasteType).style.color = balance < 0 ? 'red' : 'gray';
 
-    }
-    document.getElementById('progress_' + wasteType).value = sum;
+	var max = maxes[wasteType];
+	if (sum > max) {
+		document.getElementById('progress_' + wasteType).className = 'styled5';
+	}
+	else if (sum > 0.75*max && sum <= max) {
+		document.getElementById('progress_' + wasteType).className = 'styled4';
+	}
+	else if (sum > 0.5*max && sum <= 0.75*max) {
+		document.getElementById('progress_' + wasteType).className = 'styled3';
+	}
+	else if (sum > 0.25*max && sum <= 0.5*max) {
+		document.getElementById('progress_' + wasteType).className = 'styled2';
+	}
+	else {
+		document.getElementById('progress_' + wasteType).className = 'styled1';		
+	}
 }
 
 function plus1(wasteType){
 	var waste = 1;
 	wastes[wasteType].push(waste*1);
 	calculate(wastes, wasteType);
-	console.log(wastes);
 	save(wastes);
 }
 
@@ -53,6 +67,26 @@ function plus10(wasteType){
 	var waste = 10;
 	wastes[wasteType].push(waste*1);
 	calculate(wastes, wasteType);
-	console.log(wastes);
 	save(wastes);
 }
+
+function changeMax(wasteType){
+	var nameType = document.querySelector("#title_" + wasteType).textContent;
+	var max = prompt("Бюджет по тратам " + "'" + nameType + "'" + " составляет:");
+	max = parseInt(max);
+	if (isNaN(max)) {
+		return;
+	}
+	setMax(wasteType, max);
+	save();
+	calculate(wastes, wasteType);
+}
+
+function setMax(wasteType, max){
+	maxes[wasteType] = max; 
+	document.getElementById('progress_' + wasteType).max = max;
+	document.getElementById('max_' + wasteType).innerHTML = max;
+	// console.log(max);
+}
+
+console.log(wastes);
