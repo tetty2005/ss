56a3a469fb3738ac81eac6132ab5c32f82d414c7
Tatiana <tetty2005@mail.ru>
2017-06-14 18:59:45 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+<script src="src/task1.js"></script>
+<script src="src/task2.js"></script>
+<script src="src/task3.js"></script>
+<script src="src/task4.js"></script>
+<script src="src/task5.js"></script>
+<script src="src/task6.js"></script>
+<script src="src/task7.js"></script>
+
+
+console.log(drawBoard(10,5,'+'));
