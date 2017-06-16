3556586606fb33ad4812a45b5c00d767aeb5357d
Tatiana <tetty2005@mail.ru>
2017-06-16 11:05:08 +0300

    Update task2.js

--- src/task2.js
+++ src/task2.js
@@ -1,15 +1,22 @@
 function task2(envelope1, envelope2){
-	if (envelope1 == undefined || envelope2 == undefined) {
+
+	if (typeof envelope1 !== 'object' || typeof envelope2 !== 'object') {
+
 		return {status: 'failed', reason: 'Enter two envelopes as objects'};
-	} else if (envelope1.a == undefined || envelope1.b == undefined) {
+
+	} else if (typeof envelope1.a !== 'number' || typeof envelope1.b !== 'number' ) {
+
 		return {status: 'failed', reason: 'Enter two sides of first envelope as properties a and b'};
-	} else if (envelope2.c == undefined || envelope2.d == undefined) {
+
+	} else if (typeof envelope2.c !== 'number' || typeof envelope2.d !== 'number') {
+
 		return {status: 'failed', reason: 'Enter two sides of second envelope as properties c and d'};
+
 	} else {
+
 	   	return (envelope1.a < envelope2.c && envelope1.b < envelope2.c ||
 			envelope1.a < envelope2.d && envelope1.b < envelope2.d) ? 1 :
 			(envelope2.c < envelope1.a && envelope2.d < envelope1.b  || 
 			envelope2.d < envelope1.a && envelope2.c < envelope1.b) ? 2 : 0;
 	}
 }
-    
