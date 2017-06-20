e3eb628f36e101677b466be64df4960da617a914
Tatiana <tetty2005@mail.ru>
2017-06-20 13:14:18 +0300

    Update task4.js

--- src/task4.js
+++ src/task4.js
@@ -1,10 +1,8 @@
 function task4 (number) {
     if (typeof number === 'number') {
-        // если число подходит, преобразуем его в строку для проверки 
-        // на наличие палидрма, а результат проверки преобразуем в число
-        if (number > 10){
-            let str = String(number);
-            let result = Number(checkPalindrom(str));
+        if (number > 10){                              // Если число подходит,
+            let str = String(number);                  // преобразуем его в строку для проверки на наличие палидрма,
+            let result = Number(checkPalindrom(str));  // а результат проверки преобразуем обратно в число.
             
             return result;
 
@@ -18,17 +16,17 @@ function task4 (number) {
 function checkPalindrom (str) {
     let strReverse = str.split('').reverse().join('');
 
-    if (strReverse == str) {        // если строка является палиндромом, 
-        return str;                 // возвращаем ее
+    if (strReverse == str) {        // Если строка является палиндромом, 
+        return str;                 // возвращаем ее.
     } else {
-        return cutString(str);      // если нет, обрезаем
+        return cutString(str);      // Если нет, обрезаем ее
     }
 }
 
 function cutString (str) {
-    if (str.length > 1) {        // до тех пор, пока она будет больше 1 символа
-        let newStr1 = checkPalindrom(str.split('').slice(0,length-1).join(''));
-        let newStr2 = checkPalindrom(str.split('').slice(1).join(''));
+    if (str.length > 1) {           // до тех пор, пока она будет больше 1 символа.
+        let newStr1 = checkPalindrom(str.split('').slice(0,length-1).join(''));  // Обрезаем по 1 символу справа
+        let newStr2 = checkPalindrom(str.split('').slice(1).join(''));           // и по 1 символу слева до обнаружения палиндрома.
 
         if (Number(newStr1) > Number(newStr2)) {
             return newStr1;
