5b40dd7546635eb7490207405ea60ba71c189e99
Tatiana <tetty2005@mail.ru>
2017-06-20 12:37:59 +0300

    Update task3.js

--- src/task3.js
+++ src/task3.js
@@ -1,11 +1,12 @@
 function task3 (arrayTriangles) {
     if (typeof arrayTriangles !== 'object') {
         return  {status: 'failed', reason: 'Enter an array with triangles'};
+        
     } else if (typeof arrayTriangles[0] !== 'object' || typeof arrayTriangles[1] !== 'object') {
         return  {status: 'failed', reason: 'Enter an array with two or more triangles'};
-    } else {
         
-        let getSquare = function (triangle) {
+    } else {
+        let getSquare = function (triangle) {             // расчет площади треугольника по формуле Герона
             let triangleSides = [];
 
             for (let prop in triangle) {
@@ -18,18 +19,18 @@ function task3 (arrayTriangles) {
             return square;
         };
 
-        let getNames = function(triangle){
+        let getNames = function (triangle) {
             return triangle.vertices;
         };
 
-        let arranging = function (a,b) {
+        let arranging = function (a, b) {
             return b - a;
         };
 
-        let squares = arrayTriangles.map(getSquare);    // создаем массив площадей
-        let names = arrayTriangles.map(getNames);       // и массив названий треугольников
-        let squaresArranging = squares.slice().sort(arranging); // сортируем площади в новый массив
-        let namesArranging = [];                                // создаем искомый массив названий треугольников
+        let squares = arrayTriangles.map(getSquare);            // Создаем массив площадей
+        let names = arrayTriangles.map(getNames);               // и массив названий треугольников.
+        let squaresArranging = squares.slice().sort(arranging); // Сортируем площади в новом массиве.
+        let namesArranging = [];                                // Создаем искомый массив названий треугольников
 
         for (let i = 0; i < squaresArranging.length; i++){                      // и наполняем его 
             namesArranging.push(names[squares.indexOf(squaresArranging[i])]);   // согласно порядку площадей
