ded1fbcedf595b04c83711fb678e60885f37e066
Tatiana <tetty2005@mail.ru>
2017-06-20 12:30:41 +0300

    Update task2.js

--- src/task2.js
+++ src/task2.js
@@ -1,10 +1,13 @@
-function task2 (envelope1, envelope2){
+function task2 (envelope1, envelope2) {
     if (typeof envelope1 !== 'object' || typeof envelope2 !== 'object') {
         return {status: 'failed', reason: 'Enter two envelopes as objects'};
+        
     } else if (typeof envelope1.a !== 'number' || typeof envelope1.b !== 'number' ) {
         return {status: 'failed', reason: 'Enter two sides of first envelope as properties a and b'};
+        
     } else if (typeof envelope2.c !== 'number' || typeof envelope2.d !== 'number') {
         return {status: 'failed', reason: 'Enter two sides of second envelope as properties c and d'};
+        
     } else {
         return (envelope1.a < envelope2.c && envelope1.b < envelope2.c ||   // первый конверт меньше?
             envelope1.a < envelope2.d && envelope1.b < envelope2.d) ? 1 :   // а если повернуть?
