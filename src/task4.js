68c4e8e692c5d7e985c619954c416ab88ab1e192
Tatiana <tetty2005@mail.ru>
2017-06-20 14:09:30 +0300

    Update task4.js

--- src/task4.js
+++ src/task4.js
@@ -1,39 +1,39 @@
-function task4 (number) {
-    if (typeof number === 'number') {
-        if (number > 10){                              // Если число подходит,
+function task4 (number) {   
+    if (typeof number !== 'number') {
+        return {status: 'failed', reason: 'Enter a number'};
+
+    } else {
+    let palindrom = 0;
+
+        if (number > 10) {                             // Если число не одноцифровое,
             let str = String(number);                  // преобразуем его в строку для проверки на наличие палидрма,
-            let result = Number(checkPalindrom(str));  // а результат проверки преобразуем обратно в число.
-            
-            return result;
-
-        } else {
-            return  0;
+            palindrom = Number(checkPalindrom(str));   // а результат проверки преобразуем обратно в число.
         }
-    }
-    return {status: 'failed', reason: 'Enter a number'};
-}
 
-function checkPalindrom (str) {
-    let strReverse = str.split('').reverse().join('');
+        return palindrom;
 
-    if (strReverse == str) {        // Если строка является палиндромом, 
-        return str;                 // возвращаем ее.
-    } else {
-        return cutString(str);      // Если нет, обрезаем ее
-    }
-}
+        function checkPalindrom (str) {
+            let strReverse = str.split('').reverse().join('');
 
-function cutString (str) {
-    if (str.length > 1) {           // до тех пор, пока она будет больше 1 символа.
-        let newStr1 = checkPalindrom(str.split('').slice(0,length-1).join(''));  // Обрезаем по 1 символу справа
-        let newStr2 = checkPalindrom(str.split('').slice(1).join(''));           // и по 1 символу слева до обнаружения палиндрома.
-
-        if (Number(newStr1) > Number(newStr2)) {
-            return newStr1;
-        } else {
-            return newStr2;
+            if (strReverse === str) {        // Если строка является палиндромом, 
+                return str;                  // возвращаем ее.
+            } else {
+                return cutString(str);       // Если нет, обрезаем ее
+            }
         }
-    } else {
-        return 0;
+
+        function cutString (str) {
+                let newStr1 = checkPalindrom(str.split('').slice(0,length-1).join(''));  // Обрезаем по 1 символу справа
+                let newStr2 = checkPalindrom(str.split('').slice(1).join(''));           // и по 1 символу слева до обнаружения палиндрома.
+
+                if (Number(newStr1) >= Number(newStr2) && Number(newStr1) > 10) {        // Из двух полученных строк выбираем бОльшую
+                    palindrom = newStr1;                                                 // и присваиваем палиндрому ее значение.
+
+                } else if (Number(newStr2) > Number(newStr1) && Number(newStr2) > 10) {
+                    palindrom = newStr2;
+                }
+
+            return palindrom;
+        }
     }
 }
