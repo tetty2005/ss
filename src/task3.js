8c99ccd2a48f8f5895b02c9d092e177886c85623
Tatiana <tetty2005@mail.ru>
2017-06-22 21:40:13 +0300

    Update task3.js

--- src/task3.js
+++ src/task3.js
@@ -1,41 +1,61 @@
+'use strict'
+
 function task3 (arrayTriangles) {
-    if (typeof arrayTriangles !== 'object') {
-        return  {status: 'failed', reason: 'Enter an array with triangles'};
-        
-    } else if (typeof arrayTriangles[0] !== 'object' || typeof arrayTriangles[1] !== 'object') {
-        return  {status: 'failed', reason: 'Enter an array with two or more triangles'};
-        
+    let result;
+    let error = preValidate(arrayTriangles);
+
+    if (error === '') {
+        result = arrangeTriangles(arrayTriangles);
     } else {
-        let getSquare = function (triangle) {             // расчет площади треугольника по формуле Герона
-            let triangleSides = [];
-
-            for (let prop in triangle) {
-                triangleSides.push(triangle[prop]);
-            }
-
-            let p = (triangleSides[1] + triangleSides[2] + triangleSides[3])/2;
-            let square = Math.sqrt(p*(p - triangleSides[1])*(p - triangleSides[2])*(p - triangleSides[3]));
-
-            return square;
-        };
-
-        let getNames = function (triangle) {
-            return triangle.vertices;
-        };
-
-        let arranging = function (a, b) {
-            return b - a;
-        };
-
-        let squares = arrayTriangles.map(getSquare);            // Создаем массив площадей
-        let names = arrayTriangles.map(getNames);               // и массив названий треугольников.
-        let squaresArranging = squares.slice().sort(arranging); // Сортируем площади в новом массиве.
-        let namesArranging = [];                                // Создаем искомый массив названий треугольников
-
-        for (let i = 0; i < squaresArranging.length; i++){                      // и наполняем его 
-            namesArranging.push(names[squares.indexOf(squaresArranging[i])]);   // согласно порядку площадей
-        }
-        
-        return namesArranging;
+        result = {status: 'failed', reason: error};
     }
+
+    return result;
+}
+        
+let getSquare = function (triangle) {
+    let sides = [],
+        square, p;
+
+    for (let prop in triangle) {
+        sides.push(triangle[prop]);
+    }
+
+    p = (sides[1] + sides[2] + sides[3])/2;
+    square = Math.sqrt(p*(p - sides[1])*(p - sides[2])*(p - sides[3]));
+
+    return square;
+};
+
+let getNames = function (triangle) {
+    return triangle.vertices;
+};
+
+let arranging = function (a, b) {
+    return b - a;
+};
+
+function arrangeTriangles (arrayTriangles) {
+    let squares = arrayTriangles.map(getSquare);
+    let names = arrayTriangles.map(getNames);
+    let squaresArranging = squares.slice().sort(arranging);
+    let namesArranging = [];
+
+    for (let i = 0; i < squaresArranging.length; i++) {
+        namesArranging.push(names[squares.indexOf(squaresArranging[i])]);
+    }
+            
+    return namesArranging;
+}
+
+function preValidate (arrayTriangles) {
+    let error = '';
+
+    if (typeof arrayTriangles !== 'object') {
+        error = 'Enter an array with triangles';
+    } else if (typeof arrayTriangles[0] !== 'object' || 
+               typeof arrayTriangles[1] !== 'object') {
+        error = 'Enter an array with two or more triangles';
+    }
+    return error;
 }
