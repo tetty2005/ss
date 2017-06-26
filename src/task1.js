721adb626bba44de69a0feefc831eedcb1aa52e2
Tatiana <tetty2005@mail.ru>
2017-06-26 13:33:29 +0300

    Update task1.js

--- src/task1.js
+++ src/task1.js
@@ -1,32 +1,50 @@
-function task1 (boardX, boardY, symbol) {
-    if (typeof boardX !== 'number' || typeof boardY !== 'number' || 
-        typeof symbol !== 'string'){
+'use strict'
 
-        return  {status: 'failed', reason: 'Enter two numbers and a symbol as a string'};
-        
+function task1 (width, height, symbol) {
+    let result, error;
+    error = preValidate1(width, height, symbol);
+
+    if (!error) {
+        result = drawBoard(width, height, symbol);
     } else {
-        let line1 = '',
-            line2 = '',
-            enter = '\n',
-            space = ' ',
-            board = '';
-
-        for (let i = 0; i < boardX; i++) {          // рисуем первую строку
-            line1 += symbol + space;
-        }
-
-        for (let i = 0; i < boardX-1; i++) {        // рисуем вторую строку
-            line2 += space + symbol;
-        }           
-
-        for (let i = 1; i < boardY/2; i++) {        // рисуем доску,
-            board +=  line1 + enter + line2 + enter;// чередуя первую и вторую строки
-        }
-
-        if (boardY % 2 != 0) {          // если высота доски равна нечетному количеству строк,
-            board +=  line1 + enter;    // то добавляем еще одну строку
-        }
-
-        return board;
+        result = {status: 'failed', reason: error};
     }
+
+    return result;
+}
+
+function drawBoard (width, height, symbol) {
+    let lineEven = '',
+        lineOdd = '',
+        board = '';
+
+    for (let i = 0; i < width; i++) {
+        lineOdd += symbol + ' ';
+    }
+
+    for (let i = 0; i < width-1; i++) {
+        lineEven += ' ' + symbol;
+    }           
+
+    for (let i = 1; i < height/2; i++) {
+        board +=  lineOdd + '\n' + lineEven + '\n';
+    }
+
+    if (height % 2 != 0) {
+        board +=  lineOdd + '\n';
+    }
+
+    return board;
+}
+
+function preValidate1 (width, height, symbol) {
+    let error = '';
+
+    if (typeof width !== 'number' || 
+        typeof height !== 'number' || 
+        typeof symbol !== 'string') {
+        error = 'Enter two numbers and a symbol as a string';
+    }
+
+    return error;
 }
