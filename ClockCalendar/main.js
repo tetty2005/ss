9e8c777b7253820ed1b6023b1e48279be098dd27
Tatiana <tetty2005@mail.ru>
2017-06-27 14:19:58 +0300

    Update main.js

--- ClockCalendar/main.js
+++ ClockCalendar/main.js
@@ -1,15 +1,88 @@
-setInterval(CurrentDateTime, 1000);
+'use strict'
 
-function CurrentDateTime () {
-    document.getElementsByClassName('clock-calendar')[0].innerHTML = new Date();
-}
+document.addEventListener('DOMContentLoaded', init);
+setInterval(init, 1000);
 
-function switchFormats () {
-  // left mouse button should:
-  // for Clocks - switch formats between 'short' and 'full'
-  // for Calendar - switch formats between 'eu-date' and 'ua-date'
-}
+let mode = 'timeShort';
 
-function () {
-    // right mouse button should switch Clock to Calendar and back
+function init() {
+    let div = document.querySelector('.clock-calendar'),
+        currentTime = new Date(),
+        hh = currentTime.getHours(),
+        mm = currentTime.getMinutes(),
+        ss = currentTime.getSeconds(),
+        day = currentTime.getDate(),
+        month = currentTime.getMonth() + 1,
+        year = currentTime.getFullYear();
+        
+    setClockCalendar();
+
+    div.addEventListener('click', switchFormats);
+    div.addEventListener('contextmenu', switchClockCalendar, false);
+    div.addEventListener('mouseover', () => div.classList.add('blue'));
+    div.addEventListener('mouseout', () => div.classList.remove('blue'));
+
+    function setClockCalendar () {
+        if (mode === 'dateEU') {
+            setCalendarEU();
+        } else if (mode === 'dateUA') {
+            setCalendarUA();
+        } else if (mode === 'timeFull') {
+            setTimeFull();
+        } else if (mode === 'timeShort') {
+            setTimeShort();
+        }
+    }
+
+    function setTimeShort () {
+        console.log(String(hh).length);
+        hh = (String(hh).length < 2) ? '0'+ hh : hh;
+        mm = (String(mm).length < 2) ? '0'+ mm : mm;
+        div.innerHTML = `${hh}:${mm}`;
+        mode = 'timeShort';
+    }
+
+    function setTimeFull () {
+        hh = ((hh).length < 2) ? '0'+ hh : hh;
+        mm = ((mm).length < 2) ? '0'+ mm : mm;
+        ss = ((ss).length < 2) ? '0'+ ss : ss;
+        div.innerHTML = `${hh}:${mm}:${ss}`;
+        mode = 'timeFull';
+    }
+
+    function setCalendarEU () {
+        month = (String(month).length < 2) ? '0'+ month : month;
+        day = (String(day).length < 2) ? '0'+ day : day;
+        div.innerHTML = `${month}/${day}/${year}`;
+        mode = 'dateEU';
+    }
+
+    function setCalendarUA () {
+        month = (String(month).length < 2) ? '0'+ month : month;
+        day = (String(day).length < 2) ? '0'+ day : day;
+        div.innerHTML = `${day}.${month}.${year}`;
+        mode = 'dateUA';
+    }
+
+
+    function switchClockCalendar (e) {
+        if (mode === 'dateEU' || mode === 'dateUA') {
+            setTimeShort();
+        } else {
+            setCalendarEU();
+        }
+        e.preventDefault();
+    }
+
+    function switchFormats () {
+        if (mode === 'dateEU') {
+            setCalendarUA();
+        } else if (mode === 'dateUA') {
+            setCalendarEU();
+        } else if (mode === 'timeFull') {
+            setTimeShort();
+        } else if (mode === 'timeShort') {
+            setTimeFull();
+        }
+    }
 }
