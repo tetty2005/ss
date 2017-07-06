48e3b2de67c375f34dbcf111090df4257a24355d
Tatiana <tetty2005@mail.ru>
2017-07-06 09:32:22 +0300

    Update Pet.js

--- PetShop/app/Model/Pet.js
+++ PetShop/app/Model/Pet.js
@@ -3,4 +3,20 @@ class Pet {
 		this.color = color;
 		this.price = price;
 	}
+
+    isYourColor (_color) {
+        return this.color === _color;
+    }
+
+    areYouFluffy () {
+        return this.isFluffy;
+    }
+
+    areYouFluffyOrWhite () {
+        return this.areYouFluffy() || this.isYourColor('white');
+    }
+
+    isYourPriceBigger (_price) {
+        return this.price > _price;
+    }
 }
