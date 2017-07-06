22d6da89e5238fe7d70679da5d4625001ff5096a
Tatiana <tetty2005@mail.ru>
2017-07-06 09:31:22 +0300

    Update main.js

--- PetShop/app/main.js
+++ PetShop/app/main.js
@@ -5,13 +5,13 @@ function upload () {
 
     view.petShop.addPets(new Dog('white', 100, 'Snezhok'));
     view.petShop.addPets(new Dog('brown', 500, 'Kashtanka'));
-    view.petShop.addPets(new Cat('white', 650, 'SnowWhite'));
+    view.petShop.addPets(new Cat('white', 650, 'SnowWhite', true));
     view.petShop.addPets(new Dog('black', 300, 'Gimmy'));
-    view.petShop.addPets(new Cat('black', 550, 'Bagira'));
+    view.petShop.addPets(new Cat('black', 550, 'Bagira', true));
     view.petShop.addPets(new Hamster('white', 100, true));
     view.petShop.addPets(new Hamster('grey', 150, true));
     view.petShop.addPets(new Hamster('black', 50, false));
-    view.petShop.addPets(new Cat('red', 100, 'Reddy'));
+    view.petShop.addPets(new Cat('red', 100, 'Reddy', false));
 
     view.showOutput();
 }
