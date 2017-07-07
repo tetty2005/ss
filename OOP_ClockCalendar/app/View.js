73099160a51cb7e920b802c682cf4e4a9da4e14a
Tatiana <tetty2005@mail.ru>
2017-07-07 14:09:14 +0300

    Update View.js

--- OOP_ClockCalendar/app/View.js
+++ OOP_ClockCalendar/app/View.js
@@ -3,7 +3,7 @@ class View {
         this.div = document.querySelector('.clock-calendar');
         this.clockCalendar = new clockCalendar();
         this.mode = 'short';
-        this.currentTime = this.clockCalendar.getTimeShort();
+        this.currentTime = this.clockCalendar.getTime('short');
 
         this.div.addEventListener('click', () => this.switchFormat());
         this.div.addEventListener('contextmenu', (e) => {
@@ -14,58 +14,58 @@ class View {
     }
 
     render () {
-        this.div.innerHTML = this.clockCalendar.getTimeShort();
+        this.div.innerHTML = this.clockCalendar.getTime('short');
         this.clockCalendar.start(() => this.refresh());
     }
 
     switchFormat () {
         if (this.mode === 'short') {
             this.mode = 'full';
-            this.div.innerHTML = this.clockCalendar.getTimeFull();
+            this.div.innerHTML = this.clockCalendar.getTime('full');
         } else if (this.mode === 'full') {
             this.mode = 'short';
-            this.div.innerHTML = this.clockCalendar.getTimeShort();
+            this.div.innerHTML = this.clockCalendar.getTime('short');
         } else if (this.mode === 'ua') {
             this.mode = 'eu';
-            this.div.innerHTML = this.clockCalendar.getDateEU();
+            this.div.innerHTML = this.clockCalendar.getDate('eu');
         } else if (this.mode === 'eu') {
             this.mode = 'ua';
-            this.div.innerHTML = this.clockCalendar.getDateUA();
+            this.div.innerHTML = this.clockCalendar.getDate('ua');
         }
     }
 
     switchMode () {
         if (this.mode === 'short' || this.mode === 'full') {
             this.mode = 'ua';
-            this.div.innerHTML = this.clockCalendar.getDateUA();
+            this.div.innerHTML = this.clockCalendar.getDate('ua');
         } else if (this.mode === 'ua' || this.mode === 'eu') {
             this.mode = 'short';
-            this.div.innerHTML = this.clockCalendar.getTimeShort();
+            this.div.innerHTML = this.clockCalendar.getTime('short');
         }
     }
 
     refresh () {
         let shortTime = this.mode === 'short' 
-            && this.currentTime !== this.clockCalendar.getTimeShort(),
+            && this.currentTime !== this.clockCalendar.getTime('short'),
             fullTime = this.mode === 'full' 
-            && this.currentTime !== this.clockCalendar.getTimeFull(),
+            && this.currentTime !== this.clockCalendar.getTime('full'),
             uaDate = this.mode === 'ua' 
-            && this.currentTime !== this.clockCalendar.getDateUA(),
+            && this.currentTime !== this.clockCalendar.getDate('ua'),
             euDate = this.mode === 'eu' 
-            && this.currentTime !== this.clockCalendar.getDateEU();
+            && this.currentTime !== this.clockCalendar.getDate('eu');
         
         if (shortTime) {
-            this.div.innerHTML = this.clockCalendar.getTimeShort();
-            this.currentTime = this.clockCalendar.getTimeShort();
+            this.div.innerHTML = this.clockCalendar.getTime('short');
+            this.currentTime = this.clockCalendar.getTime('short');
         } else if (fullTime) {
-            this.div.innerHTML = this.clockCalendar.getTimeFull();
-            this.currentTime = this.clockCalendar.getTimeFull();
+            this.div.innerHTML = this.clockCalendar.getTime('full');
+            this.currentTime = this.clockCalendar.getTime('full');
         } else if (uaDate) {
-            this.div.innerHTML = this.clockCalendar.getDateUA();
-            this.currentTime = this.clockCalendar.getDateUA();
+            this.div.innerHTML = this.clockCalendar.getDate('ua');
+            this.currentTime = this.clockCalendar.getDate('ua');
         } else if (euDate) {
-            this.div.innerHTML = this.clockCalendar.getDateEU();
-            this.currentTime = this.clockCalendar.getDateEU();
+            this.div.innerHTML = this.clockCalendar.getDate('eu');
+            this.currentTime = this.clockCalendar.getDate('eu');
         }
     }
 }
