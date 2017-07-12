29d5dbe67a6badf56edc465261780fb6c9bc28c5
Tatiana <tetty2005@mail.ru>
2017-07-12 19:04:40 +0300

    Update main.js

--- Web-component/app/main.js
+++ Web-component/app/main.js
@@ -4,8 +4,4 @@ window.onload = () => upload();
 
 function upload () {
     document.registerElement('clock-calendar-widget', ClockCalendar);
-    let widget = document.querySelector('clock-calendar-widget');
-    
-    widget.start();
-    widget.render();
 }
